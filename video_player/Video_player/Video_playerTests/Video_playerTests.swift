4e58e3ca02b4416ad10367f1c278af82366a944a
Odasaku-Khan <tenseizangetsu3@gmail.com>
2024-10-28 16:18:28 +0500

    Add files via upload

---
+++ video_player/Video_player/Video_playerTests/Video_playerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_playerTests.swift
+//  Video_playerTests
+//
+//  Created by Ablaikhan Nusypakhin on 10/27/24.
+//
+
+import Testing
+@testable import Video_player
+
+struct Video_playerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ video_player/Video_player/Video_playerUITests/Video_playerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Video_playerUITestsLaunchTests.swift
+//  Video_playerUITests
+//
+//  Created by Ablaikhan Nusypakhin on 10/27/24.
+//
+
+import XCTest
+
+final class Video_playerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
